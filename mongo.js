f978cce37e5f57b791e37e812963c59f1fc4d387
grammarandsyntax <yevak2@nycstudents.net>
2024-05-21 10:19:30 -0400

    mongo test

--- mongo.js
+++ mongo.js
@@ -3,3 +3,18 @@ const {MongoClient} = require('mongodb');
 const uri = process.env.MONGODB_CONNECTION_URI;
 const client = new MongoClient(uri);
 
+async function run() {
+    try {
+      await client.connect();
+      const db = client.db('sample_mflix');
+      const collection = db.collection('movies');
+  
+      // Find the first document in the collection
+      const first = await collection.findOne();
+      console.log(first);
+    } finally {
+      // Close the database connection when finished or an error occurs
+      await client.close();
+    }
+  }
+  run().catch(console.error);
